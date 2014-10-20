f575ed8d67f66435c051c47cf416e39ed4f0e132
jethroo <cwirth79@web.de>
2014-10-20 22:51:33 +0200

    refactoring in process

--- lib/lare_round.rb
+++ lib/lare_round.rb
@@ -3,34 +3,47 @@ require 'bigdecimal'
 module LareRound
 
   def self.round(values,precision)
+    handle_input_errors(values, precision)
+    process(values, precision)
+  end
+
+  # StandardError for dealing with application level errors
+  class LareRoundError < StandardError; end
+
+  private
+
+  def self.process(values, precision)
+    values.kind_of?(Hash) ? process_hash(values, precision) : round_array_of_values(values, precision)
+  end
+
+  def self.process_hash(values, precision)
+    rounded_values = round_array_of_values(values.values, precision)
+    values.tap do |values| 
+      values.keys.each_with_index do |key,index|
+        values[key] = rounded_values[index]
+      end
+    end
+  end
+
+  def self.handle_input_errors(values, precision)
+    # although it is the senders responsibility to ensure that correct messages are sent to this module
+    # it might not be quite obvious so i provide some help here with errors if input is invalid
+
     raise LareRoundError.new("precision must not be nil")                   if precision.nil?
     raise LareRoundError.new("precision must be a number")                  unless precision.is_a? Numeric
     raise LareRoundError.new("precision must be greater or equal to 0")     if precision < 0
     raise LareRoundError.new("values must not be nil")                      if values.nil?
     raise LareRoundError.new("values must not be empty")                    if values.empty?
-    if values.kind_of?(Array)
-      round_array_of_values(values,precision)
-    elsif values.kind_of?(Hash)
-      rounded_values = round_array_of_values(values.values,precision)
-      values.keys.each_with_index do |key,index|
-        values[key] = rounded_values[index]
-      end
-      return values
-    end
-  end
 
-  # StandardError for dealing with application level errors
-  class LareRoundError < StandardError
-
-  end
-
-  private
-  def self.round_array_of_values(array_of_values,precision)
-    raise LareRoundError.new("array_of_values must be an array")            unless array_of_values.is_a? Array
+    array_of_values = values.kind_of?(Hash) ? values.values : values
+    raise LareRoundError.new("array_of_values must be an array") unless array_of_values.is_a? Array raise LareRoundError.new("array_of_values must be an array") unless array_of_values.is_a? Array
+    
     number_of_invalid_values = array_of_values.map{|i| i.is_a? Numeric}.reject{|i| i == true}.size
     raise LareRoundError.new("values contains not numeric values (#{number_of_invalid_values})") if number_of_invalid_values > 0
     warn "values contains non decimal values, you might loose precision or even get wrong rounding results" if array_of_values.map{|i| i.is_a? BigDecimal}.reject{|i| i == true}.size > 0
+  end
 
+  def self.round_array_of_values(array_of_values,precision)
     #prevention of can't omit precision for a Rational
     decimal_shift = BigDecimal.new (10 ** precision.to_i)
     rounded_total = array_of_values.reduce(:+).round(precision) * decimal_shift
@@ -50,5 +63,4 @@ module LareRound
 
     return rounded_values.map{|v| v / decimal_shift }
   end
-
 end
