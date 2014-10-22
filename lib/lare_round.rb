858ebe5cfe87efd8e79a53cd1a7e54f8c249c1e5
jethroo <cwirth79@web.de>
2014-10-22 23:29:18 +0200

    fixing rubocop errors and small refactoring

--- lib/lare_round.rb
+++ lib/lare_round.rb
@@ -2,7 +2,13 @@ require 'bigdecimal'
 
 module LareRound
   def self.round(values, precision)
-    handle_input_errors(values, precision)
+    # although it is the senders responsibility to ensure that correct messages
+    # are sent to this module it might not be quite obvious so i provide some
+    # help here with errors if input is invalid
+    array_of_values = values.is_a?(Hash) ? values.values : values
+    handle_value_errors(array_of_values)
+    handle_precision_errors(precision)
+
     process(values, precision)
   end
 
@@ -28,28 +34,43 @@ module LareRound
     end
   end
 
-  def self.handle_input_errors(values, precision)
-    # although it is the senders responsibility to ensure that correct messages are sent to this module
-    # it might not be quite obvious so i provide some help here with errors if input is invalid
+  def self.handle_value_errors(values)
+    fail LareRoundError, 'values must not be nil' if values.nil?
+    fail LareRoundError, 'values must not be empty' if values.empty?
+    fail LareRoundError, 'values must be an array' unless values.is_a? Array
 
-    raise LareRoundError.new("precision must not be nil")                   if precision.nil?
-    raise LareRoundError.new("precision must be a number")                  unless precision.is_a? Numeric
-    raise LareRoundError.new("precision must be greater or equal to 0")     if precision < 0
-    raise LareRoundError.new("values must not be nil")                      if values.nil?
-    raise LareRoundError.new("values must not be empty")                    if values.empty?
+    numbers_invalid = values.map { |i| i.is_a? Numeric }
+      .reject { |i| i == true }.size
+    if numbers_invalid > 0
+      error = <<-ERROR.strip.gsub(/\s+/, ' ')
+        values contains not numeric values (#{numbers_invalid})
+      ERROR
+      fail LareRoundError, error
+    end
 
-    array_of_values = values.kind_of?(Hash) ? values.values : values
-    raise LareRoundError.new("array_of_values must be an array") unless array_of_values.is_a? Array 
+    if values.map { |i| i.is_a? BigDecimal }.reject { |i| i == true }.size > 0
+      warning = <<-WARNING.strip.gsub(/\s+/, ' ')
+        values contains non decimal values,
+        you might loose precision or even get wrong rounding results
+      WARNING
+      warn warning
+    end
+  end
 
-    number_of_invalid_values = array_of_values.map{|i| i.is_a? Numeric}.reject{|i| i == true}.size
-    raise LareRoundError.new("values contains not numeric values (#{number_of_invalid_values})") if number_of_invalid_values > 0
+  def self.handle_precision_errors(precision)
+    fail LareRoundError, 'precision must not be nil' if precision.nil?
 
-    warning = "values contains non decimal values, you might loose precision or even get wrong rounding results"
-    warn warning if array_of_values.map{|i| i.is_a? BigDecimal}.reject{|i| i == true}.size > 0
+    unless precision.is_a? Numeric
+      fail LareRoundError, 'precision must be a number'
+    end
+
+    if precision < 0
+      fail LareRoundError, 'precision must be greater or equal to 0'
+    end
   end
 
   Struct.new(
-    "IntermediaryResults",
+    'IntermediaryResults',
     :decimal_shift,
     :rounded_total,
     :array_of_values,
@@ -61,11 +82,13 @@ module LareRound
   def self.round_array_of_values(array_of_values, precision)
     mrc = Struct::IntermediaryResults.new
     mrc.precision = precision
-    #prevention of can't omit precision for a Rational
-    mrc.decimal_shift = BigDecimal.new (10 ** precision.to_i)
-    mrc.rounded_total = array_of_values.reduce(:+).round(precision) * mrc.decimal_shift
-    mrc.array_of_values = array_of_values.map{|v| ((v.is_a? BigDecimal) ? v : BigDecimal.new(v.to_s))}
-    mrc.unrounded_values = array_of_values.map{|v| v * mrc.decimal_shift }
+    mrc.decimal_shift = BigDecimal.new(10**precision.to_i)
+    mrc.rounded_total = array_of_values.reduce(:+)
+      .round(precision) * mrc.decimal_shift
+    mrc.array_of_values = array_of_values.map do |v|
+      ((v.is_a? BigDecimal) ? v : BigDecimal.new(v.to_s))
+    end
+    mrc.unrounded_values = array_of_values.map { |v| v * mrc.decimal_shift }
 
     largest_remainder_method(mrc)
 
@@ -73,14 +96,18 @@ module LareRound
   end
 
   def self.largest_remainder_method(mrc)
-    mrc.rounded_values = mrc.array_of_values.map{|v| largest_remainder_round(v, mrc) }
+    mrc.rounded_values = mrc.array_of_values.map do |v|
+      largest_remainder_round(v, mrc)
+    end
 
-    while not mrc.rounded_values.reduce(:+) >= mrc.rounded_total
-      fractions = mrc.unrounded_values.zip(mrc.rounded_values).map { |x, y| x - y }
+    until mrc.rounded_values.reduce(:+) >= mrc.rounded_total
+      fractions = mrc.unrounded_values.zip(mrc.rounded_values).map do |x, y|
+        x - y
+      end
       mrc.rounded_values[fractions.index(fractions.max)] += 1
     end
 
-    mrc.rounded_values.map!{ |v| v / mrc.decimal_shift }
+    mrc.rounded_values.map! { |v| v / mrc.decimal_shift }
   end
 
   def self.largest_remainder_round(v, mrc)
@@ -89,10 +116,10 @@ module LareRound
     # (0.7).round(0) + (0.7).round(0) + (0.7).round(0) = 1 + 1 + 1 = 3
     # elsewise if negative
     rounding_strategy = if v < 0
-      BigDecimal::ROUND_UP
-    else
-      BigDecimal::ROUND_DOWN
-    end
-    v.round(mrc.precision, rounding_strategy ) * mrc.decimal_shift
+                          BigDecimal::ROUND_UP
+                        else
+                          BigDecimal::ROUND_DOWN
+                        end
+    v.round(mrc.precision, rounding_strategy) * mrc.decimal_shift
   end
 end
